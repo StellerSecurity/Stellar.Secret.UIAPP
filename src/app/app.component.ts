3c0513a7fcb9832ffaf8e91b032441d6a1d115c9
stellar-security-os <stellarsecaps@gmail.com>
2024-06-05 02:39:04 +0200

    Optimized for desktop.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,31 @@
-import { Component } from '@angular/core';
+import {Component, NgZone} from '@angular/core';
 import { StatusBar, Style } from '@capacitor/status-bar';
+import { Router } from '@angular/router';
+import { App, URLOpenListenerEvent } from '@capacitor/app';
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {
+  constructor(private router: Router, private zone: NgZone) {
    this.startup();
   }
 
   public startup() {
+    App.addListener('appUrlOpen', (event: URLOpenListenerEvent) => {
+      this.zone.run(() => {
+        // Example url: https://beerswift.app/tabs/tab2
+        // slug = /tabs/tab2
+        const slug = event.url.split(".app").pop();
+        if (slug) {
+          this.router.navigateByUrl(slug);
+        }
+        // If no match, do nothing - let regular routing
+        // logic take over
+      });
+    });
     StatusBar.setBackgroundColor({color: "black"}).then(r => {});
   }
 
