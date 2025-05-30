72ee69a4c888ab9a0038bacd54de0501882332a5
ketan <ketan.akbari123@gmail.com>
2025-05-30 16:11:50 +0530

    Added UI for  for privacy policy page

--- src/app/privacy-policy/privacy-policy.component.ts
+++ src/app/privacy-policy/privacy-policy.component.ts
@@ -6,7 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./privacy-policy.component.scss'],
 })
 export class PrivacyPolicyComponent  implements OnInit {
-
+  metaDescription:string = 'Share a onetime secret message and file with Stellar Secret. Protect your privacy and securely share confidential information.';
+  metaTitle:string = 'Stellar Secret | Share a OneTime Secret Message and File';
+  metaKeywords:string = 'Secret message generator, Secure message sharing, Encrypt personal information, Password protection, User data encryption, Private data sharing, Convert sensitive data';
+  url: string = 'https://stellarsecret.io/';
+  
   constructor() { }
 
   ngOnInit() {}
