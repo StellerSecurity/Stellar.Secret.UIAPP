26f05028ed4de1b11126ef97d03555743d1e2efb
Vlad <alb99888@proton.me>
2026-02-19 20:12:07 +0100

    ios

--- src/app/app/components/footer/footer.component.ts
+++ src/app/app/components/footer/footer.component.ts
@@ -1,21 +1,27 @@
+// footer.component.ts
 import { Component, OnInit } from '@angular/core';
+import { NgIf } from '@angular/common';
+import { Capacitor } from '@capacitor/core';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss'],
-  standalone: true
+  standalone: true,
+  imports: [NgIf],
 })
-export class FooterComponent  implements OnInit {
+export class FooterComponent implements OnInit {
+  showFooterLinks = true;
 
-  constructor() { }
+  ngOnInit(): void {
+    const isIOSNativeApp =
+        Capacitor.isNativePlatform() && Capacitor.getPlatform() === 'ios';
 
-  ngOnInit() {
-
+    // Hide footer links when running in Capacitor on iOS
+    this.showFooterLinks = !isIOSNativeApp;
   }
 
   navigateToUrl(url: string): void {
-    window.location.href = 'https://example.com';
+    window.location.href = url;
   }
-
 }
